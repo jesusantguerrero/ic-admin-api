3a9e8e3997361ec32208660c51538a7378732a01
Jesus Guerrero <jesusant.guerrero@gmail.com>
2019-03-18 17:27:31 -0400

    add status page

--- start/routes.js
+++ start/routes.js
@@ -18,25 +18,33 @@ const Route = use('Route')
 
 Route.on('/').render('welcome')
 
+Route.on('/api/v1/status', ({
+  response
+}) => {
+  response.json({
+    "status": "working"
+  })
+})
+
 Route.group(() => {
-    Route.resource('items', 'ItemController').apiOnly()
-    
-    // Company Section
-    Route.resource('users', 'UserController').apiOnly()
-    Route.resource('companies', 'CompanyController').apiOnly()
-    Route.resource('clients', 'ClientController').apiOnly()
-    
-    // Business
-    Route.resource('routers', 'RouterController').apiOnly()
-    Route.resource('services', 'ServiceController').apiOnly()
-    Route.resource('prices', 'PriceController').apiOnly()
-    Route.resource('contracts', 'ContractController').apiOnly()
-    Route.resource('payments', 'PaymentController').apiOnly()
-    
-    // issues section
-    Route.resource('tickets', 'TicketController').apiOnly()
-    Route.resource('labels', 'LabelController').apiOnly()
-    Route.resource('milestones', 'MilestoneController').apiOnly()
+  Route.resource('items', 'ItemController').apiOnly()
+
+  // Company Section
+  Route.resource('users', 'UserController').apiOnly()
+  Route.resource('companies', 'CompanyController').apiOnly()
+  Route.resource('clients', 'ClientController').apiOnly()
+
+  // Business
+  Route.resource('routers', 'RouterController').apiOnly()
+  Route.resource('services', 'ServiceController').apiOnly()
+  Route.resource('prices', 'PriceController').apiOnly()
+  Route.resource('contracts', 'ContractController').apiOnly()
+  Route.resource('payments', 'PaymentController').apiOnly()
+
+  // issues section
+  Route.resource('tickets', 'TicketController').apiOnly()
+  Route.resource('labels', 'LabelController').apiOnly()
+  Route.resource('milestones', 'MilestoneController').apiOnly()
 
 }).prefix('api/v1').middleware('auth')
 
@@ -49,4 +57,3 @@ Route.group(() => {
 Route.group(() => {
   Route.get('user', 'AuthController.currentUser')
 }).prefix('api/v1/auth').middleware('auth')
-
